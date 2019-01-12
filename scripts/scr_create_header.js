f12762044c61305a74d2f3a54a49b73361fa4370
IGOR CHIGUR <chigur.ig@gmail.com>
2019-01-12 18:34:29 +0200

    Initial commit

---
+++ scripts/scr_create_header.js
@@ -0,0 +1,77 @@
+function createHeader(page) {
+
+    var div = document.createElement('div');
+
+
+        div.innerHTML = "            <div class=\"row\">\n" +
+            "\n" +
+            "                <div  class=\"col-md-2 col-sm-2\" align=\"center\" >\n" +
+            "                    <a href=\"../index.html\">\n" +
+            "                        <div id=\"top_menu-1\" class=\"col-md-10 col-sm-10 offset=md-1 sm-1\" align=\"center\" >\n" +
+            "                            <p>Main</p>\n" +
+            "                            <!--<p>Main</p>-->\n" +
+            "                        </div>\n" +
+            "                    </a>\n" +
+            "                </div>\n" +
+            "                <div  class=\"col-md-2 col-sm-2\" align=\"center\" >\n" +
+            "                    <a href=\"../HTML/page_6.html\">\n" +
+            "                        <div id=\"top_menu-2\" class=\"col-md-10 col-sm-10 offset=md-1 sm-1\" align=\"center\" >\n" +
+            "\n" +
+            "                            <p>About us</p>\n" +
+            "                            <!--<p>About us</p>-->\n" +
+            "                        </div>\n" +
+            "                    </a>\n" +
+            "                </div>\n" +
+            "                <div  class=\"col-md-2 col-sm-2 \" align=\"center\" >\n" +
+            "                    <a href=\"../index.html#row_2\">\n" +
+            "                        <div id=\"top_menu-3\" class=\"col-md-10 col-sm-10 offset=md-1 sm-1\" align=\"center\" >\n" +
+            "\n" +
+            "                            <p>Services</p>\n" +
+            "                        </div>\n" +
+            "                    </a>\n" +
+            "                    <!--<p>Services</p>-->\n" +
+            "                </div>\n" +
+            "                <div  class=\"col-md-2 col-sm-2\" align=\"center\" >\n" +
+            "                    <a href=\"../HTML/page_7.html\">\n" +
+            "                        <div id=\"top_menu-4\" class=\"col-md-10 col-sm-10 offset=md-1 sm-1\" align=\"center\" >\n" +
+            "\n" +
+            "                            <p>News</p>\n" +
+            "                            <!--<p>News</p>-->\n" +
+            "                        </div>\n" +
+            "                    </a>\n" +
+            "                </div>\n" +
+            "                <div  class=\"col-md-2 col-sm-2\" align=\"center\" >\n" +
+            "                    <a href=\"../HTML/page_5.html\">\n" +
+            "                        <div id=\"top_menu-5\" class=\"col-md-10 col-sm-10 offset=md-1 sm-1\" align=\"center\" >\n" +
+            "\n" +
+            "                            <p>Contacts</p>\n" +
+            "                        </div>\n" +
+            "                    </a>\n" +
+            "                    <!--<p>Contacts</p>-->\n" +
+            "                </div>\n" +
+            "                <div  class=\"col-md-2 col-sm-2\" align=\"center\" >\n" +
+            "                    <a href=\"../HTML/page_8.html\">\n" +
+            "                        <div id=\"top_menu-6\" class=\"col-md-10 col-sm-10 offset=md-1 sm-1\" align=\"center\" >\n" +
+            "\n" +
+            "                            <p>Catalog</p>\n" +
+            "                            <!--<p>Catalog</p>-->\n" +
+            "\n" +
+            "                        </div>\n" +
+            "                    </a>\n" +
+            "                </div>\n" +
+            "            </div>\n";
+
+
+
+
+    header.appendChild(div);
+
+
+
+
+
+
+
+}
+
+
---
+++ scripts/scr_go_to_pg1.js
@@ -0,0 +1,2 @@
+window.location.href = "pages/pg1.html";
+// window.location.href = "http://www.w3schools.com";
