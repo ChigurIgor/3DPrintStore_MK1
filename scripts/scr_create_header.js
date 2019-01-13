cbf9c08b0160440017492ee3fe185866d85657f4
IGOR CHIGUR <chigur.ig@gmail.com>
2019-01-13 23:52:10 +0200

    added cartHtml added cart button to header

--- scripts/scr_create_header.js
+++ scripts/scr_create_header.js
@@ -5,18 +5,25 @@ function createHeader(page) {
 
         div.innerHTML =
         " <div class=\"row\">\n" +
-            "                <div class=\"col-2 offset-1\">\n" +
+            "                <div id=\"logo\" class=\"col-2 offset-1\">\n" +
             "                   <a href=\"main.html\"> <img src=\"images/support/icon_logo.png\"></a>\n" +
             "                </div>\n" +
             "                <div class=\"col-3\">\n" +
             "                    <a href=\"main.html\"> <h1>3DPrint Store</h1></a>\n" +
             "                </div>\n" +
             "\n" +
-            "                <div class=\"col-3\">\n" +
+            "                <div id=\"mDeliv\" class=\"col-3\">\n" +
             "                    <a href=\"delivery.html\"> <p>Shipping and payment</p></a>\n" +
             "                </div>\n" +
+            "                <div id=\"mCart\" class=\"col-3\">\n" +
+            "<div class='row'>\n"+
+            "                   <a href=\"cart.html\"> <img src=\"images/support/icon_cart.png\"></a>\n" +
+            "                   <a href=\"cart.html\"><p>Your cart</p></a>\n"+
+            "</div>\n" +
+            "</div>\n" +
+            "                </div>\n" +
             "            </div>"
-            ;
+                    ;
 
 
 
