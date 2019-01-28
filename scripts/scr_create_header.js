2796da73c2989be29780e21202d62b2b3dd76b90
IGOR CHIGUR <chigur.ig@gmail.com>
2019-01-28 13:23:39 +0200

    working on mobile

--- scripts/scr_create_header.js
+++ scripts/scr_create_header.js
@@ -15,29 +15,25 @@ function createHeader(page) {
             "                <div id=\"logo\" class=\"col-3\">\n" +
             "                   <a href=\"main.html\"> <img src=\"images/support/icon_logo.png\"></a>\n" +
             "                </div>\n" +
-            "                <div class=\"col-2\">\n" +
+            "                <div class=\"col-6\">\n" +
             "                    <a href=\"main.html\"> <h1>3DPrintMan</h1></a>\n" +
             "                </div>\n" +
             "\n" +
 
-            // "                <div id=\"mCart\" class=\"col-3\">\n" +
-            // "<div id=\"mCartRow\" class='row'>\n" +
-            // "                <div id=\"mCartCol\" class=\"col-12\">\n" +
-            // "<div id=\"\" class='row'>\n" +
-            //
-            // "                   <a href=\"cart.html\"> <img src=\"images/support/icon_cart.png\"></a>\n" +
-            //
-            // "</div>\n" +
-            // "<div id=\"\" class='row'>\n" +
-            //
-            // "                   <a href=\"cart.html\"><p>Сart</p></a>\n" +
-            //
-            // "</div>\n" +
-            //
-            // "</div>\n" +
-            // "</div>\n" +
-            // "</div>\n" +
-            // "                </div>\n" +
+            "                <div id=\"mCart\" class=\"col-3\">\n" +
+            "<div id=\"mCartRow\" class='row'>\n" +
+            "                <div id=\"mCartCol\" class=\"col-12\">\n" +
+            "<div id=\"\" class='row'>\n" +
+
+            "                   <a href=\"cart.html\"> <img src=\"images/support/icon_cart.png\"></a>\n" +
+
+            "</div>\n" +
+
+
+            "</div>\n" +
+            "</div>\n" +
+            "</div>\n" +
+            "                </div>\n" +
             "            </div>"
         ;
 
