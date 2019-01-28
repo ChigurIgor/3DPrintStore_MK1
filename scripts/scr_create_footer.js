0935f9d0e92486db07bb231e0ed9641623cb4103
IGOR CHIGUR <chigur.ig@gmail.com>
2019-01-28 12:32:32 +0200

    working on mobile

--- scripts/scr_create_footer.js
+++ scripts/scr_create_footer.js
@@ -4,18 +4,41 @@ function createFooter(page) {
                 div.className="row";
                 div.id="footerRow";
 
+    if( navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)){
+
         div.innerHTML =
-            "<div class='col-2' id='cont-1'><p>Tel: 054 904 2727</p></div>"+
-            "<div class='col-4' id='cont-2'><a href='#'><img src='./images/support/icon_facebook.png'></a></div>"+
-           " <div id=\"mDeliv\" class=\"col-4\">\n" +
-    "                    <a href=\"delivery.html\"> <p>Delivery and Payment</p></a>\n" +
-    "                </div>\n"
+            "<div class='col-6' id='cont-1'><p>Tel: 054 904 2727</p></div>"
+            +
+            "<div class='col-4' id='cont-2'><a href='#'><img src='./images/support/icon_facebook.png'></a></div>"
+            // +
+            // " <div id=\"mDeliv\" class=\"col-4\">\n" +
+            // "                    <a href=\"delivery.html\"> <p>Delivery and Payment</p></a>\n" +
+            // "                </div>\n"
 
         ;
 
 
+    }
+    else {
 
 
+        div.innerHTML =
+            "<div class='col-2' id='cont-1'><p>Tel: 054 904 2727</p></div>" +
+            "<div class='col-4' id='cont-2'><a href='#'><img src='./images/support/icon_facebook.png'></a></div>" +
+            " <div id=\"mDeliv\" class=\"col-4\">\n" +
+            "                    <a href=\"delivery.html\"> <p>Delivery and Payment</p></a>\n" +
+            "                </div>\n"
+
+        ;
+
+
+    }
     footer.appendChild(div);
 
 
